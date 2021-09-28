abe1bcc66f7458ba3eddc4c5d9c25434c13bd140
liris <zzk@live.com>
2021-09-28 23:02:39 +0800

    update

--- data/custom_dataset.py
+++ data/custom_dataset.py
@@ -1,22 +1,27 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
-import sys
-sys.path.append("..")
-from utils.tools import sliding_windows, preprocessing
+from NetworkTrafficPrediction.utils.tools import sliding_windows, preprocessing
 
 
 class LteTrafficDataset(Dataset):
-    def __init__(self, file_path, CellName, windows_size, target_size, transform=None, target_transform=None):
+    def __init__(
+            self,
+            file_path: str,
+            CellName: str,
+            windows_size: int,
+            target_size: int,
+            transform=None,
+            target_transform=None):
         self.df = pd.read_csv(file_path)
         self.CellName = CellName
         self.transform = transform
         self.target_transform = target_transform
         self.windows_size = windows_size
         self.target_size = target_size
-        self.dataframe = preprocessing(self.df,self.CellName)
-        self.history_values, self.pred_values = sliding_windows(self.dataframe, self.dataframe.values,
-                                                                self.windows_size, self.target_size)
+        self.dataframe = preprocessing(self.df, self.CellName)
+        self.history_values, self.pred_values = sliding_windows(
+            self.dataframe, self.dataframe.values, self.windows_size, self.target_size)
 
     def __len__(self):
         return len(self.history_values)
@@ -25,16 +30,23 @@ class LteTrafficDataset(Dataset):
         history_value = self.history_values[idx]
         pred_value = self.pred_values[idx]
         if self.transform:
-            history_value = torch.tensor(history_value,dtype=torch.float32) / self.dataframe.values.max()
+            history_value = torch.tensor(
+                history_value, dtype=torch.float32) / self.dataframe.values.max()
         if self.target_transform:
-            pred_value = torch.tensor(pred_value,dtype=torch.float32) / self.dataframe.values.max()
-        return history_value,pred_value
+            pred_value = torch.tensor(
+                pred_value, dtype=torch.float32) / self.dataframe.values.max()
+        return history_value, pred_value
 
 
 if __name__ == '__main__':
     file_path = '../dataset/LteTraffic/lte.train.csv'
-    training_data = LteTrafficDataset(file_path, CellName='Cell_003781', windows_size=12,
-                                      target_size=1, transform=True, target_transform=True)
+    training_data = LteTrafficDataset(
+        file_path,
+        CellName='Cell_003781',
+        windows_size=12,
+        target_size=1,
+        transform=True,
+        target_transform=True)
     train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
     X, y = next(iter(train_dataloader))
-    print(X.shape,y.shape)
+    print(X.shape, y.shape)
--- utils/tools.py
+++ utils/tools.py
@@ -1,21 +1,39 @@
 import pandas as pd
 
 
-def sliding_windows(time_values,data_values,windows_size,target_size,start_index=0):
+def sliding_windows(
+        time_values,
+        data_values,
+        windows_size: int,
+        target_size: int,
+        start_index=0) -> list:
     X = []
     y = []
     for i in range(len(time_values)):
         X.append(data_values[start_index:start_index + windows_size])
-        y.append(data_values[start_index + windows_size:start_index + windows_size + target_size])
+        y.append(data_values[start_index +
+                             windows_size:start_index +
+                             windows_size +
+                             target_size])
         start_index += 1
     return X, y
 
-def preprocessing(data_frame,CellName):
+
+def preprocessing(data_frame, CellName):
     data_frame = data_frame[data_frame['CellName'] == CellName].copy()
     data_frame.loc[:, 'Date'] = pd.to_datetime(data_frame.Date.astype(str))
     data_frame.loc[:, 'Hour'] = pd.to_timedelta(data_frame.Hour, unit='h')
-    data_frame.loc[:, 'Date_time'] = pd.to_datetime(data_frame.Date + data_frame.Hour)
+    data_frame.loc[:, 'Date_time'] = pd.to_datetime(
+        data_frame.Date + data_frame.Hour)
     data_frame = data_frame.drop(['Date', 'Hour', 'CellName'], axis=1)
     data_frame = data_frame.set_index('Date_time')
     data_frame = data_frame.sort_values(by='Date_time')
     return data_frame
+
+
+if __name__ == '__main__':
+    import numpy as np
+    x = np.arange(100)
+    X, y = sliding_windows(x, x, 12, 1)
+    print('Test-sliding-windows-function')
+    print(len(X), '\n', X[0], y[0], '\n', X[1], y[1])
