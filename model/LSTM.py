82e45a05f86b3bd0a596617a5f90ba3c93a553b0
liris <zzk@live.com>
2021-10-07 13:25:44 +0800

    update

--- model/LSTM.py
+++ model/LSTM.py
@@ -5,18 +5,19 @@ import torch.nn.functional as F
 
 class LSTM(nn.Module):
     def __init__(
-            self,
-            window_size,
-            input_feature=1,
-            target_size=1,
-            batch_size=64,
-            hidden_size=20,
-            num_lstm=1):
+        self,
+        window_size,
+        input_feature=1,
+        target_size=1,
+        batch_size=64,
+        hidden_size=20,
+        num_lstm=1,
+            device='cpu'):
         super(LSTM, self).__init__()
         self.LSTM = nn.LSTM(input_feature, hidden_size, num_lstm)
         self.hidden_memory, self.cell_memory = torch.zeros(
-            num_lstm, batch_size, hidden_size), torch.zeros(
-            num_lstm, batch_size, hidden_size)
+            num_lstm, batch_size, hidden_size).to(device), torch.zeros(
+            num_lstm, batch_size, hidden_size).to(device)
         self.flatten = nn.Flatten()
         self.linear1 = nn.Linear(
             hidden_size * window_size, int(hidden_size * window_size / 2))
