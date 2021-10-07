b9a37110dbdea1986592350ab6ee86c9c7824b5e
liris <zzk@live.com>
2021-10-07 15:31:03 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# NetworkTrafficPrediction
- Using deep learning methods to predict network traffic(such as Lte..)
+# Network Traffic Prediction
+
+> Using **deep learning** methods to predict **network traffic**(such as Lte..)
+- ## Model Task
+- [x] DNN
+- [ ] CNN1d
+- [ ] RNN
+- [x] LSTM
+- [ ] GRU
+- [x] Transformer
+- ## Code supplement
+- [ ] Model save
+- [ ] Dynamic Learning rate
+- [ ] ...
--- model/LSTM.py
+++ model/LSTM.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class LSTM(nn.Module):
+class LSTMNetwork(nn.Module):
     def __init__(
         self,
         window_size,
@@ -13,7 +13,7 @@ class LSTM(nn.Module):
         hidden_size=20,
         num_lstm=1,
             device='cpu'):
-        super(LSTM, self).__init__()
+        super(LSTMNetwork, self).__init__()
         self.LSTM = nn.LSTM(input_feature, hidden_size, num_lstm)
         self.hidden_memory, self.cell_memory = torch.zeros(
             num_lstm, batch_size, hidden_size).to(device), torch.zeros(
@@ -39,6 +39,6 @@ class LSTM(nn.Module):
 
 if __name__ == '__main__':
     input = torch.randn(64, 12, 1)
-    net = LSTM(window_size=12)
+    net = LSTMNetwork(window_size=12)
     out = net(input)
     print(out.shape)
