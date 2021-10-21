05447b4eab892a9efa1948c402ebaa3e79394b1c
liris <zzk@live.com>
2021-10-22 00:49:09 +0800

    fix bugs

--- model/LSTM.py
+++ model/LSTM.py
@@ -14,9 +14,14 @@ class LSTMModule(nn.Module):
             num_lstm: int = 1,
             device: str = 'cpu',
             dropout: float = 0,
-            bidirectional:bool = False):
+            bidirectional: bool = False):
         super(LSTMModule, self).__init__()
-        self.LSTM = nn.LSTM(input_feature, hidden_size, num_lstm,dropout=dropout,bidirectional=bidirectional)
+        self.LSTM = nn.LSTM(
+            input_feature,
+            hidden_size,
+            num_lstm,
+            dropout=dropout,
+            bidirectional=bidirectional)
         self.hidden_memory, self.cell_memory = torch.zeros(
             num_lstm, batch_size, hidden_size).to(device), torch.zeros(
             num_lstm, batch_size, hidden_size).to(device)
@@ -43,7 +48,7 @@ if __name__ == '__main__':
     windows_size = 120
     target_size = 12
     input = torch.randn(64, windows_size, 1)
-    net = LSTMModule(windows_size, 1, 48 ,64, 200, 2)
+    net = LSTMModule(windows_size, 1, 48, 64, 200, 2)
     out = net(input)
     print(net)
     print(out.shape)
--- utils/tools.py
+++ utils/tools.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch
+import numpy as np
 
 
 def sliding_windows(
@@ -108,69 +109,93 @@ def Tr_accuracy(y_pred: float, y_true: float, tolerate_rate: int) -> float:
     return ((y_true * (1 - tolerate_rate) < y_pred) == (y_pred < (1 + tolerate_rate)
             * y_true)).sum() / (y_pred.shape[0] * y_pred.shape[1] * y_pred.shape[2])
 
-# fork from https://clay-atlas.com/blog/2020/09/29/pytorch-cn-early-stopping-code/
-def train_test_model(
-        train_loader,
-        test_loader,
-        net,
-        optimizer,
-        train_loss_fn,
-        test_loss_fn,
-        device,
-        Tr_rate,
-        epochs,
-        patience):
-    test_loss = 0
-    test_acc = 0
-    last_loss = 100
-    trigger_times = 0
-    for t in range(1, epochs):
-        net.train()
-        for batch, (X, y) in enumerate(train_loader):
-            X, y = X.to(device), y.to(device)
+# copyright @ https://github.com/Bjarten/early-stopping-pytorch/blob/master/MNIST_Early_Stopping_example.ipynb
+def train_model(train_loader,
+                valid_loader,
+                net,
+                optimizer,
+                train_loss_fn,
+                test_loss_fn,
+                device,
+                n_epochs,
+                patience):
+    # to track the training loss as the model trains
+    train_losses = []
+    # to track the validation loss as the model trains
+    valid_losses = []
+    # to track the average training loss per epoch as the model trains
+    avg_train_losses = []
+    # to track the average validation loss per epoch as the model trains
+    avg_valid_losses = []
+
+    # initialize the early_stopping object
+    early_stopping = EarlyStopping(patience=patience, verbose=True)
+
+    for epoch in range(1, n_epochs + 1):
+
+        ###################
+        # train the model #
+        ###################
+        net.train()  # prep model for training
+        for batch, (X, y) in enumerate(train_loader, 1):
+            X,y = X.to(device),y.to(device)
+            # clear the gradients of all optimized variables
             optimizer.zero_grad()
-            pred = net(X)
-            loss = train_loss_fn(pred, y)
+            # forward pass: compute predicted outputs by passing inputs to the model
+            output = net(X)
+            # calculate the loss
+            loss = train_loss_fn(output, y)
+            # backward pass: compute gradient of the loss with respect to model parameters
             loss.backward()
+            # perform a single optimization step (parameter update)
             optimizer.step()
+            # record training loss
+            train_losses.append(loss.item())
 
-            if batch % 100 == 0 or batch == len(train_loader):
-                print(
-                    f'Epochs:{t} / {epochs}: \nMSE loss{loss.item()}    [{batch}/ {len(train_loader)}] \n')
+        ######################
+        # validate the model #
+        ######################
+        net.eval()  # prep model for evaluation
+        for X2, y2 in valid_loader:
+            X2,y2 = X2.to(device),y2.to(device)
+            # forward pass: compute predicted outputs by passing inputs to the model
+            output = net(X2)
+            # calculate the loss
+            loss = test_loss_fn(output, y2)
+            # record validation loss
+            valid_losses.append(loss.item())
 
-        net.eval()
-        with torch.no_grad():
-            for X2, y2 in test_loader:
-                X2,y2 = X2.to(device),y2.to(device)
-                pred = net(X2)
-                loss2 = test_loss_fn(pred, y2)
-                test_loss += loss2.item()
-                if Tr_rate != 0:
-                    test_acc += Tr_accuracy(pred, y2, Tr_rate)
+        # print training/validation statistics
+        # calculate average loss over an epoch
+        train_loss = np.average(train_losses)
+        valid_loss = np.average(valid_losses)
+        avg_train_losses.append(train_loss)
+        avg_valid_losses.append(valid_loss)
 
-        current_loss = test_loss / len(test_loader)
-        current_acc = test_acc / len(test_loader)
+        epoch_len = len(str(n_epochs))
 
-        if Tr_rate != 0:
-            print(
-                f'MAE loss{current_loss} {Tr_rate * 100}% Accuracy:{current_acc}')
-        else:
-            print(f'MAE loss{current_loss}')
-        # early stopping
+        print_msg = (f'[{epoch:>{epoch_len}}/{n_epochs:>{epoch_len}}] ' +
+                     f'train_loss: {train_loss:.5f} ' +
+                     f'valid_loss: {valid_loss:.5f}')
 
-        if current_loss > last_loss:
-            trigger_times += 1
-            print('trigger times:', trigger_times)
+        print(print_msg)
 
-            if trigger_times >= patience:
-                print('Early stopping!\nStart to test process.')
-                return net
+        # clear lists to track next epoch
+        train_losses = []
+        valid_losses = []
 
-        else:
-            print('trigger times: 0')
-            trigger_times = 0
+        # early_stopping needs the validation loss to check if it has decresed,
+        # and if it has, it will make a checkpoint of the current model
+        early_stopping(valid_loss, net)
 
-        last_loss = current_loss
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+
+    # load the last checkpoint with the best model
+    net.load_state_dict(torch.load('checkpoint.pt'))
+
+    return net, avg_train_losses, avg_valid_losses
 
 
 def preprocess_prediction(net, dataloader, device):
@@ -185,6 +210,55 @@ def preprocess_prediction(net, dataloader, device):
                 Y_pred.append(j.to('cpu').detach())
     return Y, Y_pred
 
+# copyright @ https://github.com/Bjarten/early-stopping-pytorch
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+            trace_func (function): trace print function.
+                            Default: print
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
 
 if __name__ == '__main__':
     # import numpy as np
